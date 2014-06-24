ffd6f81aad05d9169ed5d9a89420fde1e03165f6
Stefan Miklosovic <smikloso@redhat.com>
2014-06-24 16:39:21 +0200

    recorder

--- src/test/java/org/arquillian/droidium/jeeconf/AeroGearTestCase.java
+++ src/test/java/org/arquillian/droidium/jeeconf/AeroGearTestCase.java
@@ -30,6 +30,8 @@ import org.arquillian.droidium.jeeconf.fragment.web.LoginWebFragment;
 import org.arquillian.droidium.jeeconf.fragment.web.ProjectFragment;
 import org.arquillian.droidium.jeeconf.fragment.web.TaskWebFragment;
 import org.arquillian.droidium.native_.api.Instrumentable;
+import org.arquillian.droidium.native_.webdriver.AndroidDriver;
+import org.arquillian.recorder.reporter.ReportMessage;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
@@ -43,7 +45,9 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -51,24 +55,28 @@ import org.openqa.selenium.support.FindBy;
 
 @RunWith(Arquillian.class)
 @RunAsClient
+@ReportMessage("Browser and Android testing in one test case")
 public class AeroGearTestCase {
 
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    
     @Drone
     @Browser
-    WebDriver browser;
+    private WebDriver browser;
 
     @Drone
     @Mobile
-    WebDriver mobile;
+    private AndroidDriver mobile;
 
-    @Deployment(name = "todo-mobile-app")
+    @Deployment(name = "todo-mobile-app", testable = false)
     @Instrumentable(viaPort = 8081)
     @TargetsContainer("android")
     public static JavaArchive getAndroidDeployment() {
         return ShrinkWrap.createFromZipFile(JavaArchive.class, new File("android-todos.apk"));
     }
 
-    @Deployment(name = "todo-ear-app")
+    @Deployment(name = "todo-ear-app", testable = false)
     @TargetsContainer("jbossas")
     public static EnterpriseArchive getJBossASDeployment() {
         return ShrinkWrap.createFromZipFile(EnterpriseArchive.class, new File("todo-ear.ear"));
@@ -101,6 +109,7 @@ public class AeroGearTestCase {
     @Test
     @InSequence(1)
     @OperateOnDeployment("todo-ear-app")
+    @ReportMessage("Opens web application in a browser")
     public void loginUserInWebClient(@ArquillianResource URL context) {
         openWebPageUrl(browser, context);
         loginFragment.login("john", "123");
@@ -119,6 +128,7 @@ public class AeroGearTestCase {
     @Test
     @InSequence(3)
     @OperateOnDeployment("todo-ear-app")
+    @ReportMessage("Adds a task to web client")
     public void addTask() {
 
         taskFragment.addTask("groceries", "buy some milk", "2020", "10", "20", "buy some fresh milk around the corner");
@@ -138,9 +148,7 @@ public class AeroGearTestCase {
     @OperateOnDeployment("todo-mobile-app")
     public void loginUserInMobile(@ArquillianResource AndroidDevice device) {
 
-        device.getActivityManagerProvider()
-            .getActivityManager()
-            .startActivity("org.jboss.aerogear.todo.activities.LoginActivity");
+        mobile.startActivity("org.jboss.aerogear.todo.activities.LoginActivity");
 
         loginMobilePage.login("john", "123");
     }
@@ -174,4 +182,19 @@ public class AeroGearTestCase {
         logoutButton.click();
     }
 
+    @Test
+    @InSequence(9)
+    @OperateOnDeployment("todo-ear-app")
+    public void fakeException() throws Exception {
+        exception.expect(RuntimeException.class);
+        throw new RuntimeException("this is some expected exception");
+    }
+    
+    @Test(expected = RuntimeException.class)
+    @InSequence(10)
+    @OperateOnDeployment("todo-ear-app")
+    public void fakeException2() throws Exception {
+        throw new RuntimeException();
+    }
+    
 }
